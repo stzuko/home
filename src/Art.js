95767fe259f4c0224ce0c397840d1342f7184202
stzuko <sto.lat.syd@gmail.com>
2018-11-06 20:34:18 -0500

    Added art page

--- src/Art.js
+++ src/Art.js
@@ -4,27 +4,46 @@ export default class Art extends React.Component {
 	constructor(){
 		super();
 		this.state = {
-			art: [
-				{'src':'0000.jpg','alt':''},
-				{'src':'0001.jpg','alt':''},
-				{'src':'0002.jpg','alt':''}, 
-				{'src':'0003.jpg','alt':''},
-				{'src':'0004.jpg','alt':''},
-				{'src':'0005.jpg','alt':''},
-				{'src':'0006.jpg','alt':''},
-				{'src':'0007.jpg','alt':''},
-				{'src':'0008.jpg','alt':''},
-			]
+			art: 27,
+			pos: 27
+		}
+	}
+
+	createValidLink(str){
+		return '/home/img/art/' + ('0000'+str).slice(-4) + '.jpg';	
+	}
+
+	showNext(){
+		if (this.state.pos>1) {
+			this.setState({pos:this.state.pos-1});
+		} else { // Pos is at 1; reset to top
+			this.setState({pos:27});
+		}
+	}
+
+	showPrev(){
+		if (this.state.pos<this.state.art){
+			this.setState({pos:this.state.pos+1});
+		} else { // Pos is at highest, reset to 1st
+			this.setState({pos:1});
 		}
 	}
 
 	render() {	
-		return (
-		<art>
+		return ( 
     		<div className='container elegant-color-dark'>
-      			Coming Soon
-    		</div>
-  		</art>
+      			<div className='row button-span'>
+					<button onClick={this.showPrev.bind(this)} className="show-more">
+						<i class="fas fa-caret-left"></i>
+					</button>
+					<button onClick={this.showNext.bind(this)} className="show-more">
+						<i class="fas fa-caret-right"></i>
+					</button>	
+				</div>
+				<div className='row'>
+					<img src={this.createValidLink(this.state.pos)} className='art-img'></img>
+    			</div>
+			</div>
 		);
 	}
 }
