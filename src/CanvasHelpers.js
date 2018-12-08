823df2897e618fd9026460f98f3afeaa08baf658
stzuko <sto.lat.syd@gmail.com>
2018-12-08 17:52:58 -0500

    add model functions

--- src/CanvasHelpers.js
+++ src/CanvasHelpers.js
@@ -28,3 +28,34 @@ export function loadCanvas(raw) {
 	ret = Uint8Array.from(ret);
 	return ret;	
 }
+
+export function createConvModel() {
+	const model = tf.sequential();
+	model.add(tf.layers.conv2d({
+		inputShape: [IMAGE_H, IMAGE_W, 1],
+		kernelSize: 3,
+		filters: 16,
+		activation: 'relu'
+	}));
+	model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
+	model.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
+  	model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
+  	model.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
+	model.add(tf.layers.flatten({}));
+	model.add(tf.layers.dense({units: 64, activation: 'relu'}));
+	model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
+  	return model;
+}
+
+export function runModelTrain(model,images,labels) {
+	let batchSize = 1;
+	model.compile({
+		optimizer: 'rmsprop',
+		loss: 'categoricalCrossentropy',
+		metrics: ['accuracy'],
+ 	});	
+	model.fit(images, labels, { batchSize:1 }).then( ()=> {
+		console.log("fitted");
+	});
+	
+}
